b993b2bbe99b2cfac3577f0eb6f097bcef866c9a
emmagemal <emmagemal@gmail.com>
2021-10-18 14:30:39 +0200

    Emma redid analysis with type 3 error

--- Scripts/statistics.R
+++ Scripts/statistics.R
@@ -33,9 +33,11 @@ lichen_sum <- lichen %>%
 # normality of data distribution 
 hist(lichen_sum$avg_sp)   # not normal
 hist(lichen_sum$avg_cov)   # not normal
+hist(lichen_sum$avg_dia)
 
 shapiro.test(lichen_sum$avg_sp)    # normal, less power with small sample sizes
 shapiro.test(lichen_sum$avg_cov)   # normal
+shapiro.test(lichen_sum$avg_dia)   # normal
 
 # homoscedasticity 
 plot(lm(avg_sp ~ type, data = lichen_sum))
@@ -54,67 +56,61 @@ leveneTest(lichen_sum$avg_cov, lichen_sum$location)   # equal variances
 # normality of residuals 
 lm1 <- lm(avg_sp ~ type, data = lichen_sum)
 lm2 <- lm(avg_cov ~ type, data = lichen_sum)
+lm3 <- lm(avg_sp ~ location, data = lichen_sum)
+lm4 <- lm(avg_cov ~ location, data = lichen_sum)
+lm5 <- lm(avg_sp ~ avg_dia, data = lichen_sum)
+lm6 <- lm(avg_cov ~ avg_dia, data = lichen_sum)
+
 shapiro.test(residuals(lm2))  # normal
 shapiro.test(residuals(lm2))  # normal 
+shapiro.test(residuals(lm3))  # normal
+shapiro.test(residuals(lm4))  # normal
+shapiro.test(residuals(lm5))  # normal
+shapiro.test(residuals(lm6))  # normal
 
 
 ### Doing a 2-way ANOVA ----
 ## species richness 
-twoway <- aov(avg_sp ~ type + location, data = lichen_sum)
-int <- aov(avg_sp ~ type*location, data = lichen_sum)
+twoway <- lm(avg_sp ~ type + location, data = lichen_sum)
+int <- lm(avg_sp ~ type*location, data = lichen_sum)
 
 AIC(twoway, int)  # interaction is not best (twoway is best)
 
 # making a model with diameter
-twoway_dia <- aov(avg_sp ~ type + location + avg_dia, data = lichen_sum)
-int_dia <- aov(avg_sp ~ type*location*avg_dia, data = lichen_sum)
+twoway_dia <- lm(avg_sp ~ type + location + avg_dia, data = lichen_sum)
+int_dia <- lm(avg_sp ~ type*location*avg_dia, data = lichen_sum)
 
 AIC(twoway, twoway_dia, int_dia)  # int is too complicated, twoway and twoway_dia the same
                                   # twoway is simpler = maybe better?
 
-summary(twoway)
-summary(twoway_dia)  # this model explains slightly more of the variation
-# nothing is significant though
-
-# for model with tree diameter: 
-# matrix type vs richness: p = 0.350, DF = 1, F = 0.948
-# location in forest vs richness: p = 0.433, DF = 1, F = 0.658
-# diameter vs richness: p = 0.240, DF = 1, F = 1.530
-# residual variance: DF = 12, sum of squares = 12.664 (not sure if relevant)
+Anova(twoway, type = "III")
+# matrix type vs richness: p = 0.3574, DF = 1, F = 0.9105
+# location in forest vs richness: p = 0.4408, DF = 1, F = 0.6323
+# residual variance: DF = 13, lots of within-group variance that's unexplained
 
 
 ## coverage 
-twoway_cov <- aov(avg_cov ~ type + location, data = lichen_sum)
-int_cov <- aov(avg_cov ~ type*location, data = lichen_sum)
-twoway_dia_cov <- aov(avg_cov ~ type + location + avg_dia, data = lichen_sum)
-int_dia_cov <- aov(avg_cov ~ type*location*avg_dia, data = lichen_sum)
+twoway_cov <- lm(avg_cov ~ type + location, data = lichen_sum)
+int_cov <- lm(avg_cov ~ type*location, data = lichen_sum)
+twoway_dia_cov <- lm(avg_cov ~ type + location + avg_dia, data = lichen_sum)
+int_dia_cov <- lm(avg_cov ~ type*location*avg_dia, data = lichen_sum)
 
 AIC(twoway_cov, int_cov, twoway_dia_cov, int_dia_cov)   # interaction with all is best!
 
-summary(int_dia_cov)  # nothing is significant here either 
-
-# plotting the predicted values to visualize the interaction 
-fit <- lm(avg_cov ~ type*location*avg_dia, data = lichen_sum)
-plot_model(fit, type = "pred", terms = c("type", "avg_dia", "location"))
-
-
-# basic plot of coverage vs diameter in order to interpret interaction term 
-(cov_dia <- ggplot(lichen_sum, aes(x = avg_dia, y = avg_cov)) +
-              geom_point(aes(color = location)) +
-              geom_smooth(method = "lm") +
-              facet_wrap(~type, scales = "free"))
-
-(cov_dia <- ggplot(lichen_sum, aes(x = avg_dia, y = avg_cov)) +
-    geom_point(aes(color = type)) +
-    geom_smooth(method = "lm") +
-    facet_wrap(~location, scales = "free"))
-
-ggsave("Figures/coverage_diameter_plot.png", plot = cov_dia, width = 8, height = 4.5, units = "in")
+Anova(int_dia_cov, type = "III")  # nothing is significant here either 
+# matrix type vs coverage: p = 0.97956, DF = 1, F = 0.0007
+# location vs coverage: p = 0.10021, DF = 1, F = 3.4530
+# diameter vs coverage: p = 0.04856 (***SIGNIFICANT***), DF = 1, F = 5.4046
+# interaction (matrix type:location): p = 0.65435, DF = 1, F = 0.2162
+# interaction (matrix type:diameter): p = 0.89309, DF = 1, F = 0.0192
+# interaction (location:diameter): p = 0.10314, F = 1, F = 3.3834
+# interaction (all): p = 0.83066, DF = 1, F = 0.0488
+# residual variance: DF = 8
 
 
 ### Calculating summarized data for results ----
-data_sum <- lichen %>% 
+data_sum <- lichen_sum %>% 
               group_by(type, location) %>% 
-              summarize(avg_sp = mean(sp_richness),
-                        avg_cov = mean(coverage_perc))
+              summarize(avg_sp = mean(avg_sp),
+                        avg_cov = mean(avg_cov))
 
